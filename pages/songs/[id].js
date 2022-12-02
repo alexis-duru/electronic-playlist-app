35af04d760868996e463da78572eaf8076be62c1
Alexis Duru <74984922+alexis-duru@users.noreply.github.com>
2022-12-02 18:58:09 +0100

    [STYLE] [id] page of the songs

--- pages/songs/[id].js
+++ pages/songs/[id].js
@@ -1,5 +1,7 @@
 import { playlist } from '../../data';
 import Link from 'next/link';
+import arrow from '../../public/assets/img/arrow.png';
+import Image from 'next/image';
 
 export const getStaticProps = async ({ params }) => {
   const playlistList = playlist.filter((p) => p.id.toString() === params.id);
@@ -20,17 +22,21 @@ export const getStaticPaths = async () => {
 
 export default ({ song }) => (
   <>
-    <p>{song.name}</p>
-    <p>{song.artist}</p>
-    <iframe
-      width='100%'
-      height='315'
-      src={`https://www.youtube.com/embed/${song.youtubeID}`}
-      allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
-      allowFullScreen
-    />
-    <Link href='/' passHref>
-      <button as='a'>Back</button>
-    </Link>
+  <section>
+    <div className="container container__id">
+      <h1>{song.name}</h1>
+      <h3>{song.artist}</h3>
+      <iframe
+        width='100%'
+        height='315'
+        src={`https://www.youtube.com/embed/${song.youtubeID}`}
+        allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+        allowFullScreen
+      />
+      <Link href='/' passHref>
+        <button as='a'><span><Image src={arrow} height={200} width={200}></Image></span>Back</button>
+      </Link>
+    </div>
+  </section>
   </>
 );
